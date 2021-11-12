aa198d507c2b7ef26332c023b7b2c332763a5728
mas-alissa <maswebdesigener@gmail.com>
2021-11-12 19:30:20 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// part A & B:
+const IRockArrowFunctions = () => console.log("Joe, I rock arrow functions!");
+
+IRockArrowFunctions()
+
+const fivePlusSeven = () => console.log(5 + 7)
+     
+
+fivePlusSeven();
+// part C
+
+const myFunction = (a,b) => console.log(a + b);
+
+myFunction(5,3)
+
+// part D
+const addFive = a => console.log(a + 5);
+
+addFive(6)
+
+// part E
+
+const createObject = () => ({greeting:"hi"});
+
+createObject()
